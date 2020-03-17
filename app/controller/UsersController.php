161f7bdf22d651326c98c349c31128dc1787a7d7
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-17 14:30:04 +0800

    提交登陆接口
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- app/controller/UsersController.php
+++ app/controller/UsersController.php
@@ -3,21 +3,80 @@
 namespace app\controller;
 
 use app\BaseController;
+use think\Session;
+use think\Cookie;
 use app\model\UsersModel as User;
 
 class UsersController extends BaseController
 {
     public function login()
     {
-        $data['user_name'] = input('post.user_name');
-        $data['user_password'] = input('post.user_password');
+    	$user_name = input('post.userName');
+    	$user_pwd = input('post.password');
+    	
+    	if(!$user_name || !$user_pwd) 
+    	{
+	    	return  json([
+	            'code' => -1001,
+	            'msg' => '缺少参数',
+	            'data' => [
+	            		'user_name' => $user_name,
+	            		'user_pwd' => $user_pwd,
+	            	]
+	        ]);
+    	}
 
-        // $user = 
-
+    	$loginData = [
+    			'user_name' => $user_name,
+    			'user_pwd' => $user_pwd,
+    		];
+    		
+		if($user = $this->doLog($loginData))
+		{
+			return  json([
+	            'code' => 1,
+	            'msg' => '登陆成功',
+	            'data' => [
+	            		'user_nickname' => $user['user_nickname'],
+	            	]
+	        ]);
+		}
         return  json([
-            'code' => 1,
-            'msg' => '获取成功',
+            'code' => -2001,
+            'msg' => '登陆失败，用户名或密码错误',
             'data' => ''
         ]);
+    }
+    
+    public function logout()
+    {
+		//TODO 注销操作
+		return  json([
+            'code' => 1,
+            'msg' => '注销成功',
+            'data' => ''
+        ]);
+        
+    }
+    
+    public function doLog($data)
+    {
+	    $user_name = addslashes(trim(stripslashes($data['user_name'])));
+        $user_pwd = addslashes(trim(stripslashes($data['user_pwd'])));
+        
+        $loginData = [
+			['user_name', '=', $user_name],
+			['user_pwd', '=', $user_pwd],
+		];
+        if($user = User::login($loginData))
+        {
+			//TODO 登陆操作
+			session('[start]');
+			session('user_nickname', $user['user_nickname']);
+			session('user_name', $user['user_name']);
+			setcookie("jrsToken", 'gaojunhua98', time()+3600, "/", "127.0.0.1");
+            return $user;
+        }
+		return false;
     }
 }
