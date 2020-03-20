758d48cbf2d434e00ac1f7f223d0e2802664d8e2
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-20 15:16:58 +0800

    修改封装
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- app/model/ModelModel.php
+++ app/model/ModelModel.php
@@ -23,32 +23,31 @@ class ModelModel extends Model
     //根据条件查询单条
     public static function findOne($where)
     {
-    	return UserModel::where($where)->find();
+    	return ModelModel::where($where)->find();
     }
 
     //根据条件查询多条
     public static function selectAny($where)
     {
-    	return Db::name('user')->where($where)->select()->toArray();
+    	return ModelModel::where($where)->select()->toArray();
     }
 
     //新增单条
     public static function addOne($data)
     {
-    	return Db::name('user')->save($data);
+    	return ModelModel::save($data);
     }
 
     //根据条件修改单条
     public static function updateOne($where, $data)
     {
-    	return Db::name('user')->where($where)->save($data);
+    	return ModelModel::where($where)->save($data);
     }
 
     //列表页方法
     public static function getList($where, $pageData)
     {
-        $list = Db::name('user')
-                    ->where($where)
+        $list = ModelModel::where($where)
                     ->paginate($pageData['pageNum'], false, [
                         'type'     => 'Bootstrap',
                         'var_page' => 'page',
--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -20,42 +20,5 @@ class UserModel extends ModelModel
     {
         //TODO:初始化内容
     }
-    
-    //根据条件查询单条
-    // public static function findOne($where)
-    // {
-    // 	return UserModel::where($where)->find();
-    // }
 
-    //根据条件查询多条
-    public static function selectAny($where)
-    {
-    	return Db::name('user')->where($where)->select()->toArray();
-    }
-
-    //新增单条
-    public static function addOne($data)
-    {
-    	return Db::name('user')->save($data);
-    }
-
-    //根据条件修改单条
-    public static function updateOne($where, $data)
-    {
-    	return Db::name('user')->where($where)->save($data);
-    }
-
-    //列表页方法
-    public static function getList($where, $pageData)
-    {
-        $list = Db::name('user')
-                    ->where($where)
-                    ->paginate($pageData['pageNum'], false, [
-                        'type'     => 'Bootstrap',
-                        'var_page' => 'page',
-                        'page' => $pageData['page'],
-            
-                     ]);
-        return $list;
-    }
 }
