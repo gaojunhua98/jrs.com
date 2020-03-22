a07fc236d443812b04377c7c858179bc6cede178
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-22 16:59:01 +0800

    111
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- public/index.php
+++ public/index.php
@@ -12,9 +12,25 @@
 // [ 应用入口文件 ]
 namespace think;
 
-header("Access-Control-Allow-Origin:*");
-header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
-header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
+// header("Access-Control-Allow-Origin:*");
+// header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
+// header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS,PATCH');
+    file_put_contents('option.txt',json_encode($_REQUEST));
+    exit;
+}
+
+header('Access-Control-Allow-Origin:*');
+// 响应类型
+header('Access-Control-Allow-Methods:*');
+// 响应头设置
+header('Access-Control-Allow-Headers:content-type,token,id');
+header("Access-Control-Request-Headers: Origin, X-Requested-With, content-Type, Accept, Authorization");
+
 require __DIR__ . '/../vendor/autoload.php';
 
 // 执行HTTP应用并响应
