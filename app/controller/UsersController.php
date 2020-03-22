e4667601bb80b765dd8799a7f0adc7f8bdb672b1
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-22 14:49:01 +0800

    111
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

---
+++ app/controller/UsersController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\controller;
+
+use app\common\tool\RequestTool;
+use app\common\logicalentity\User;
+use app\controller\ControllerController;
+
+/**
+ * @name 用户相关接口
+ */
+class UsersController extends ControllerController
+{
+	/**
+	 * @name 登陆接口
+	 */
+    public function login()
+    {
+    	$userName = RequestTool::postParameters('userName');
+		$password = RequestTool::postParameters('password');
+    	
+    	if(!$userName || !$password) 
+    	{
+	    	return  json([
+	            'code' => -1001,
+	            'msg' => '缺少参数',
+	            'data' => [
+	            		'userName' => $userName,
+	            		'password' => $password,
+	            	]
+	        ]);
+    	}
+
+    	$loginData = [
+    			'userName' => $userName,
+    			'password' => $password,
+    		];
+    		
+		if($user = User::doLogin($loginData))
+		{
+			return  json([
+	            'code' => 1,
+	            'msg' => '登陆成功',
+	            'data' => [
+	            		'user_nickname' => $user['user_nickname'],
+	            	]
+	        ]);
+		}
+        return  json([
+            'code' => -2001,
+            'msg' => '登陆失败，用户名或密码错误',
+            'data' => ''
+        ]);
+    }
+    
+    public function logout()
+    {
+    	$user_name = RequestTool::postParameters('userName');
+		if(User::doLogout($user_name))
+		{
+			return  json([
+				'code' => 1,
+				'msg' => '注销成功',
+				'data' => ''
+			]);
+		}
+		return  json([
+            'code' => 1,
+            'msg' => '注销失败',
+            'data' => ''
+        ]);
+        
+	}
+	
+}
