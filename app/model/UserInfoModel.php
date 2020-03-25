8d4b844c1195b354c88b023646dc82e40637ea60
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-25 20:53:49 +0800

    11 Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- app/model/UserInfoModel.php
+++ app/model/UserInfoModel.php
@@ -28,9 +28,10 @@ class UserInfoModel extends ModelModel
     //列表页方法
     public function getUserInfoList($pageData)
     {
-        $list = Db::name('user_info userInfo')
+        $list = Db::name('user_info')
+                    ->alias('userInfo')
+                    ->join('jrs_user user', 'user.user_id = userInfo.user_id')
                     ->field('user.user_email,user.user_phone,userInfo.user_nickname,userInfo.user_sex,userInfo.user_birth,userInfo.user_education,userInfo.user_address,userInfo.department_id,userInfo.department_name,userInfo.user_position')
-                    ->join('jrs_user user ON user.user_id = userInfo.user_id')
                     ->paginate($pageData['pageNum'], false, [
                         'type'     => 'Bootstrap',
                         'var_page' => 'page',
