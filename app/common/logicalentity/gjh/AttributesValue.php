3d7add2bccf9c77a0864eb4458b29664bb442c0b
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-31 16:13:11 +0800

    111
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

---
+++ app/common/logicalentity/gjh/AttributesValue.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\common\logicalentity\gjh;
+
+use think\Session;
+use think\Cookie;
+use app\model\gjh\AttributesModel;
+use app\model\gjh\AttributesValueModel;
+
+/**
+ * @name 属性值相关逻辑层
+ */
+class AttributesValue
+{
+    
+}
