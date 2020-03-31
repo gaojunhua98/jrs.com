5ee5ad274fa31308feae28ba2da17c17de928c20
gaojunhua98 <gaojunhua98@gmail.com>
2020-03-31 17:05:10 +0800

    111
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- app/common/logicalentity/gjh/Attributes.php
+++ app/common/logicalentity/gjh/Attributes.php
@@ -18,7 +18,7 @@ class Attributes
     public function doGetAttributesList($where, $pageData)
     {
         $attributesInfo = AttributesModel::getList($where, $pageData);
-        var_dump($attributesInfo[data]);
+        var_dump($attributesInfo);
         die;
         foreach($attributesInfo->data as &$one)
         {
