86c829a7804a2d30519ffb09a3505c6621c4c56e
gaojunhua98 <gaojunhua98@gmail.com>
2020-04-05 18:40:21 +0800

    11
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- app/common/logicalentity/gjh/GoodsSku.php
+++ app/common/logicalentity/gjh/GoodsSku.php
@@ -241,7 +241,7 @@ class GoodsSku
             foreach($attributesValueInfos as $oneAttributesValue)
             {
                 $value = [];
-                $value[] = ['value' => $oneAttributesValue['attributes_value']];
+                $value['value'] = $oneAttributesValue['attributes_value'];
                 $values[] = $value;
             }
             $selectList[] = [
