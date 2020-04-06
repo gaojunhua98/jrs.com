603da41e086e953429bb79072c19572ae79662b8
gaojunhua98 <gaojunhua98@gmail.com>
2020-04-06 10:33:40 +0800

    111
    Signed-off-by: gaojunhua98 <gaojunhua98@gmail.com>

--- app/controller/GjhController.php
+++ app/controller/GjhController.php
@@ -543,7 +543,11 @@ class GjhController extends ControllerController
 	public function saveGoodsSku()
 	{
 		$goodsSkuId = RequestTool::postParameters('goods_sku_id');
-		$saveInfo = RequestTool::postParameters('saveInfo');
+		$goodsName = RequestTool::postParameters('goods_name');
+		$skuNum = RequestTool::postParameters('sku_num');
+		$shopId = RequestTool::postParameters('shop_id');
+		$depositoryId = RequestTool::postParameters('depository_id');
+		$values = RequestTool::postParameters('values');
 		if(empty($saveInfo)) 
 		{
 			return  json([
@@ -551,11 +555,22 @@ class GjhController extends ControllerController
 				'msg' => '缺少参数',
 				'data' => [
 						'goods_sku_id' => $goodsSkuId,
-						'saveInfo' => $saveInfo,
+						'goods_name' => $goodsName,
+						'sku_num' => $skuNum,
+						'shop_id' => $shopId,
+						'depository_id' => $depositoryId,
+						'values' => $values
 					]
 			]);
 		}
 		$goodsSkuObj = new GoodsSku();
+		$saveInfo = [
+			'goods_name' => $goodsName,
+			'sku_num' => $skuNum,
+			'shop_id' => $shopId,
+			'depository_id' => $depositoryId,
+			'values' => $values
+		];
 		if(empty($goodsSkuId))
 		{
 			$res = $goodsSkuObj->doCreateGoodsSku($saveInfo);
